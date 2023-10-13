b0d6e2a2733a308ffe1d717d8817bef691ef7848
Hasti-babaei <beingbabaei@gmail.com>
2023-10-13 12:02:25 +0330

    Cleaning Codes

--- slider.js
+++ slider.js
@@ -4,41 +4,23 @@ class slider {
   constructor(options) {
     this.options = options;
     this.intialStuff();
-
     this.createNextAndPrevBtns();
     this.createDots();
-
     this.showSlides(1);
   }
 
   intialStuff() {
     let { el: sliderElement, slideClass, auto } = this.options;
-
     if (!sliderElement) throw Error("slider element is not exists");
     Number.isInteger(auto) ? (this.auto = auto) : (this.auto = 0);
-
-    this.sliders = [...sliderElement.children].filter((elm) =>
-      elm.classList.contains(slideClass)
-    );
+    this.sliders = [...sliderElement.children].filter((elm) =>elm.classList.contains(slideClass));
   }
 
   createNextAndPrevBtns() {
     let { el: sliderElement } = this.options;
-
-    sliderElement.insertAdjacentHTML(
-      "beforeend",
-      `
-            <a class="next">&#10095;</a>
-            <a class="prev">&#10094;</a>
-        `
-    );
-
-    sliderElement
-      .querySelector(".next")
-      .addEventListener("click", () => this.incrementSlide());
-    sliderElement
-      .querySelector(".prev")
-      .addEventListener("click", () => this.decrementSlide());
+    sliderElement.insertAdjacentHTML("beforeend",`<a class="next">&#10095;</a> <a class="prev">&#10094;</a> `);
+    sliderElement.querySelector(".next").addEventListener("click", () => this.incrementSlide());
+    sliderElement.querySelector(".prev").addEventListener("click", () => this.decrementSlide());
   }
 
   incrementSlide = () => this.showSlides((this.slideIndex += 1));
@@ -47,17 +29,11 @@ class slider {
 
   createDots() {
     let { el: sliderElement } = this.options;
-
-    let dotElements = [...this.sliders].map(
-      (slider, index) => `<span class="dot" data-slide="${index + 1}"></span>`
-    );
-
+    let dotElements = [...this.sliders].map((slider, index) => `<span class="dot" data-slide="${index + 1}"></span>`);
     let dots = document.createElement("div");
     dots.classList.add("dots");
     dots.innerHTML = `${dotElements.join("")}`;
-
     sliderElement.after(dots);
-
     this.dots = dots.querySelectorAll(".dot");
     this.dots.forEach((dot) =>
       dot.addEventListener("click", (e) =>
@@ -68,18 +44,14 @@ class slider {
 
   showSlides(number) {
     let { el: sliderElement, slideClass, currentSlider } = this.options;
-
     if (number > this.sliders.length) this.slideIndex = 1;
     if (number < 1) this.slideIndex = this.sliders.length;
-
     sliderElement
       .querySelector(`.${slideClass}.active`)
       .classList.remove("active");
     this.dots.forEach((dot) => dot.classList.remove("active"));
-
     this.sliders[this.slideIndex - 1].classList.add("active");
     this.dots[this.slideIndex - 1].classList.add("active");
-
     if (currentSlider) currentSlider(this.sliders[this.slideIndex - 1]);
   }
 }
